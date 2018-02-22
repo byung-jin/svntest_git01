e27b417a31dd3d5b8e8e129aa8fe21bd236e9b40
HK_Lab4 <HK_Lab4@192.168.4.111>
2018-02-22 17:18:12 +0900

    dto클래스 생성

---
+++ testproject_git01/src/com/hk/dtos/HkDto.java
@@ -0,0 +1,14 @@
+package com.hk.dtos;
+
+import java.util.Date;
+
+public class HkDto {
+
+	private int seq;
+	private String id;
+	private String name;
+	private String title;
+	private String content;
+	private Date regDate;
+	
+}
