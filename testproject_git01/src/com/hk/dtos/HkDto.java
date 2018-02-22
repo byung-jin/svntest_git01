5652184e25473d263fca2888d93e8c50f94208d8
HK_Lab4 <HK_Lab4@192.168.4.111>
2018-02-22 17:32:04 +0900

    dto에 생성자오버로딩

--- testproject_git01/src/com/hk/dtos/HkDto.java
+++ testproject_git01/src/com/hk/dtos/HkDto.java
@@ -14,6 +14,15 @@ public class HkDto {
 		super();
 		// TODO Auto-generated constructor stub
 	}
+	public HkDto(int seq, String id, String name, String title, String content, Date regDate) {
+		super();
+		this.seq = seq;
+		this.id = id;
+		this.name = name;
+		this.title = title;
+		this.content = content;
+		this.regDate = regDate;
+	}
 	
 	
 }
